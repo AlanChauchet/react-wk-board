2217dbd2e3dfd109efdbbd81270a58d2cffc61d0
DESKTOP-QRL6JPA\Alan <alan.chauchet@gmail.com>
2018-06-23 21:22:34 +0200

    ADD redirection when trying to fetch a non existing board or page is not found

--- src/pages/Board/index.js
+++ src/pages/Board/index.js
@@ -4,6 +4,7 @@ import React, { PureComponent } from 'react';
 import { compose } from 'redux';
 import { firestoreConnect, isLoaded } from 'react-redux-firebase';
 import { connect } from 'react-redux';
+import { Redirect } from '@reach/router';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import withStyles from '@material-ui/core/styles/withStyles';
 
@@ -12,12 +13,15 @@ import BoardHeader from '../../components/BoardHeader';
 import Lists from '../../containers/Lists';
 import type { Board } from '../../types/Board';
 import type { List } from '../../types/List';
+import { BASE_BOARD_ID } from '../../router';
 
 type Props = {
   classes: any,
   boardId: string,
   board?: Board,
   lists?: List[],
+  fetchingBoard: boolean,
+  boardFetched: boolean,
 };
 
 class BoardPage extends PureComponent<Props> {
@@ -28,35 +32,46 @@ class BoardPage extends PureComponent<Props> {
   );
 
   render() {
-    const { classes, board, lists } = this.props;
+    const { classes, board, lists, fetchingBoard, boardFetched } = this.props;
 
-    const isBoardLoaded = isLoaded(this.props.board);
     const isListLoaded = isLoaded(this.props.lists);
 
-    return isBoardLoaded ? (
+    if (!fetchingBoard && boardFetched && !board) {
+      return <Redirect to={`/board/${BASE_BOARD_ID}`} noThrow />;
+    }
+
+    return !fetchingBoard ? (
       <div>
-        <BoardHeader board={board}/>
+        <BoardHeader board={board} />
         <div className={classes.content}>
-          {isListLoaded ? <Lists lists={lists}/> : this.renderLoading()}
+          {isListLoaded ? <Lists lists={lists} /> : this.renderLoading()}
         </div>
       </div>
-    ) : this.renderLoading();
+    ) : (
+      this.renderLoading()
+    );
   }
 }
 
 export default compose(
   withStyles(styles),
-  firestoreConnect((props: Props) => [{
-    collection: 'boards',
-    doc: props.boardId,
-  }, {
-    collection: 'lists',
-    storeAs: `lists_${props.boardId}`,
-    orderBy: 'order',
-    where: ['boardId', '==', props.boardId],
-  }]),
+  firestoreConnect((props: Props) => [
+    {
+      collection: 'boards',
+      doc: props.boardId,
+    },
+    {
+      collection: 'lists',
+      storeAs: `lists_${props.boardId}`,
+      orderBy: 'order',
+      where: ['boardId', '==', props.boardId],
+    },
+  ]),
   connect((state, props) => ({
-    board: state.firestore.data.boards && state.firestore.data.boards[props.boardId],
+    board:
+      state.firestore.data.boards && state.firestore.data.boards[props.boardId],
     lists: state.firestore.ordered[`lists_${props.boardId}`],
+    fetchingBoard: state.firestore.status.requesting[`boards/${props.boardId}`],
+    boardFetched: state.firestore.status.requested[`boards/${props.boardId}`],
   }))
 )(BoardPage);
--- src/router/index.js
+++ src/router/index.js
@@ -7,14 +7,19 @@ import Board from '../pages/Board';
 
 type Props = {};
 
+export const BASE_BOARD_ID = 'QJxEQGGjaGSbqb2P7U8X';
+
+const AutoRedirect = () => <Redirect to={`board/${BASE_BOARD_ID}`} noThrow />;
+
 export class MainRouter extends PureComponent<Props> {
   render() {
     return (
       <Router>
         <Auth path="/">
           <Board path="board/:boardId" />
+          <AutoRedirect default />
         </Auth>
-        <Redirect from="/" to="board/QJxEQGGjaGSbqb2P7U8X" noThrow />
+        <Redirect from="/" to={`board/${BASE_BOARD_ID}`} noThrow />
       </Router>
     );
   }
